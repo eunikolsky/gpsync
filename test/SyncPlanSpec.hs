91ff3fd742644ff9736b7d95259a5da43225ffad
Eugene <e@egeek.me>
2024-03-13 19:32:35 +0200

    Clarify `synced` => `existing` to follow the function

--- test/SyncPlanSpec.hs
+++ test/SyncPlanSpec.hs
@@ -6,17 +6,17 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "getSyncPlan" $ do
-    it "removes all non-new episodes" $
-      let synced = ["1/a.mp3", "podcast/episode.mp3", "1/b.mp3"]
+    it "removes all existing episodes" $
+      let existing = ["1/a.mp3", "podcast/episode.mp3", "1/b.mp3"]
           new = []
-          toDelete = Delete <$> synced
-      in getSyncPlan new synced `shouldBe` toDelete
+          toDelete = Delete <$> existing
+      in getSyncPlan new existing `shouldBe` toDelete
 
     it "copies all new episodes" $
-      let synced = []
+      let existing = []
           new = ["1/a.mp3", "podcast/episode.mp3", "1/b.mp3"]
           toCopy = Copy <$> new
-      in getSyncPlan new synced `shouldBe` toCopy
+      in getSyncPlan new existing `shouldBe` toCopy
 
     it "returns empty list for empty inputs" $
       getSyncPlan [] [] `shouldBe` []
